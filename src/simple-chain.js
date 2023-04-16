591bd2d2e245ad5c69a2049773baff2b32acec34
Faizieva Venera <v.faizyeva@gmail.com>
2023-04-17 00:03:58 +0300

    feat: add solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -17,19 +17,24 @@ const chainMaker = {
     return this
   },
   removeLink(position) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if ((!(typeof position == 'number')) || (isNaN(position)) || (position < 1) || (position > this.getLength())) {
+      throw new Error('You can\'t remove incorrect link!')
+      }else{
+    this.chain.splice(position-1, 1)
+    return this
+      }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse()
+   return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  let result = this.chain.join('~~')
+  this.chain = []
+  return result
+    
   }
 };
-
 module.exports = {
   chainMaker
 };
